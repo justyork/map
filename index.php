29fd3a967ad52bcad6e509143c1e5b9d45c8d0de
Denis <banxamer@gmail.com>
2014-06-24 20:45:38 +0900

    test 3

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
-// я Юрко!
+// я Дениска!
+// я программистка!
 require_once($yii);
 Yii::createWebApplication($config)->run();
