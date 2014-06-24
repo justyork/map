c8448f5ee9d3a965c0fb61b5ca98d6651624c79b
JustYork <yorkshp@gmail.com>
2014-06-24 20:01:56 +0900

    Clear engine !!

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
-
+// я Юрко!
 require_once($yii);
 Yii::createWebApplication($config)->run();
