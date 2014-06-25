79ec24342f2966608b605ee6ecc009ffa08b7fc6
BanXamer <banxamer@gmail.com>
2014-06-25 11:06:32 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 // remove the following line when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 // я Дениска!
-// я программистка!
+// я программист!
 require_once($yii);
 Yii::createWebApplication($config)->run();
